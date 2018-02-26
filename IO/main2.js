40ac5d91a069f70e03081e88ade44067ca29db15
aiwosuoai1989 <aiwosuoai1989020@163.com>
2018-02-26 11:31:52 +0800

    读写IO操作

--- IO/main2.js
+++ IO/main2.js
@@ -1,15 +1,15 @@
-var fs=require('fs');
-var data='';
-var rs=fs.createReadStream('./a.txt');
-var ws=fs.createWriteStream('b.txt');
+var fs = require('fs');
+var data = '';
+var rs = fs.createReadStream('./a.txt');
+var ws = fs.createWriteStream('b.txt');
 rs.setEncoding('utf-8');
 //监听当有数据流入的时候
-rs.on('data',function (chunc) {
+rs.on('data', function (chunc) {
     // data+=chunc;
     console.log('...');
-    ws.write(chunc,'utf-8');
+    ws.write(chunc, 'utf-8');
 });
-rs.on('end',function () {
+rs.on('end', function () {
     console.log('没有数据了');
     ws.end();
 });
